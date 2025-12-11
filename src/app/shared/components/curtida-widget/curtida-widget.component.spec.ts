85c1e980d399ef455edca2ea80d95ef903cefd14
FVS-MBA <feliven@gmail.com>
2025-12-11 20:24:40 -0300

    adiciona testes sobre método darCurtida emitir variável "curtida" dentro do debounceTime

--- src/app/shared/components/curtida-widget/curtida-widget.component.spec.ts
+++ src/app/shared/components/curtida-widget/curtida-widget.component.spec.ts
@@ -1,4 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { CurtidaWidgetComponent } from './curtida-widget.component';
@@ -39,18 +44,86 @@ describe(CurtidaWidgetComponent.name, () => {
     expect(component.id).toEqual(idExistente);
   });
 
-  it(`método darCurtida deve emitir variável "curtida"`, (done) => {
+  it(`método darCurtida deve emitir variável "curtida" 1 vez dentro do debounceTime`, (done) => {
+    fixture.detectChanges();
+
+    const spyCurtida = spyOn(component.curtida, 'emit');
+
+    component.adicionarCurtida();
+    component.adicionarCurtida();
+    component.adicionarCurtida();
+    component.adicionarCurtida();
+    component.adicionarCurtida();
+
+    setTimeout(() => {
+      expect(spyCurtida).toHaveBeenCalledTimes(1);
+      done();
+    }, 500);
+  });
+
+  it(`método darCurtida deve emitir variável "curtida" 1 vez dentro do debounceTime - com fakeAsync`, fakeAsync(() => {
+    fixture.detectChanges();
+
+    const spyCurtida = spyOn(component.curtida, 'emit');
+
+    component.adicionarCurtida();
+    component.adicionarCurtida();
+    component.adicionarCurtida();
+    component.adicionarCurtida();
+    component.adicionarCurtida();
+
+    tick(500);
+
+    expect(spyCurtida).toHaveBeenCalledTimes(1);
+  }));
+
+  it(`método darCurtida deve emitir variável "curtida" X vezes a cada X períodos de debounceTime - com fakeAsync`, fakeAsync(() => {
+    fixture.detectChanges();
+
+    const spyCurtida = spyOn(component.curtida, 'emit');
+
+    component.adicionarCurtida();
+    tick(500);
+    component.adicionarCurtida();
+    tick(500);
+    component.adicionarCurtida();
+    tick(500);
+    component.adicionarCurtida();
+    tick(500);
+    component.adicionarCurtida();
+    tick(500);
+
+    expect(spyCurtida).toHaveBeenCalledTimes(5);
+  }));
+
+  it(`método darCurtida não deve emitir nada antes do debounceTime de 500ms`, (done) => {
     fixture.detectChanges();
 
     const spyCurtida = spyOn(component.curtida, 'emit');
 
     component.adicionarCurtida();
 
+    const debounceTime = Math.floor(Math.random() * 500);
+
     setTimeout(() => {
-      expect(spyCurtida).toHaveBeenCalled();
+      expect(spyCurtida).not.toHaveBeenCalled();
       done();
-    }, 600);
+    }, debounceTime);
   });
+
+  it(`método darCurtida não deve emitir nada antes do debounceTime de 500ms - com fakeAsync`, fakeAsync(() => {
+    fixture.detectChanges();
+
+    const spyCurtida = spyOn(component.curtida, 'emit');
+
+    component.adicionarCurtida();
+
+    const debounceTime = Math.floor(Math.random() * 500);
+
+    tick(debounceTime);
+
+    expect(spyCurtida).not.toHaveBeenCalled();
+  }));
 
   // it('', () => {});
 });
