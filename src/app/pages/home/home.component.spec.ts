2c7ce11810164893219891120a41603bf2c35ed6
FVS-MBA <feliven@gmail.com>
2025-12-20 23:04:15 -0300

    cria testes para HomeComponent sobre exibir fotos da API e loader/spinner

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,11 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
 
 import { HomeComponent } from './home.component';
-import { provideHttpClient } from '@angular/common/http';
+import { Foto } from '../../interfaces/foto';
+import { AlbumFotosService } from '../../services/album-fotos.service';
+
+function criarMockFotos(tamanho: number): Foto[] {
+  const mockFotos: Foto[] = Array.from({ length: tamanho }, (_, i) => ({
+    id: i + 1,
+    url: `https://example.com/${i + 1}.jpg`,
+    description: `Foto ${i + 1}`,
+  }));
+
+  return mockFotos;
+}
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let service: AlbumFotosService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,10 +29,38 @@ describe('HomeComponent', () => {
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    service = TestBed.inject(AlbumFotosService);
   });
 
   it('should create', () => {
+    fixture.detectChanges();
+
     expect(component).toBeTruthy();
   });
+
+  it('deve exibir fotos quando chegarem da API', () => {
+    const mockFotos = criarMockFotos(5);
+
+    spyOn(service, 'getFotos').and.returnValue(of(mockFotos));
+
+    fixture.detectChanges();
+
+    const album = fixture.nativeElement.querySelector('app-album-fotos');
+    const loader = fixture.nativeElement.querySelector('.loader');
+
+    expect(album).not.toBeNull();
+    expect(loader).toBeNull();
+  });
+
+  it('deve exibir loader enquanto fotos não chegarem da API', () => {
+    fixture.detectChanges();
+
+    const album = fixture.nativeElement.querySelector('app-album-fotos');
+    const loader = fixture.nativeElement.querySelector('.loader');
+
+    expect(album).toBeNull();
+    expect(loader).not.toBeNull();
+  });
+
+  // it('',()=>{})
 });
