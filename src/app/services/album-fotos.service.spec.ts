bf4462a3627d6ff173210bdd14695064fed2d0b7
FVS-MBA <feliven@gmail.com>
2025-12-21 10:51:14 -0300

    cria testes para AlbumFotosService

--- src/app/services/album-fotos.service.spec.ts
+++ src/app/services/album-fotos.service.spec.ts
@@ -1,19 +1,67 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AlbumFotosService } from './album-fotos.service';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
+import { Foto } from '../interfaces/foto';
 import { provideHttpClient } from '@angular/common/http';
+
+function criarMockFotos(tamanho: number): Foto[] {
+  const mockFotos: Foto[] = Array.from({ length: tamanho }, (_, i) => ({
+    id: i + 1,
+    url: `https://example.com/${i + 1}.jpg`,
+    description: `Foto ${i + 1}`,
+  }));
+
+  return mockFotos;
+}
+
+function criarMockDados(tamanho: number) {
+  return {
+    api: 'http://localhost:3000/photos',
+    dados: criarMockFotos(tamanho),
+  };
+}
 
 describe('AlbumFotosService', () => {
   let service: AlbumFotosService;
+  let httpController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [provideHttpClient()],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     });
     service = TestBed.inject(AlbumFotosService);
+    httpController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  fit('deve retornar fotos com descrição em maiúscula', (done) => {
+    // Math.floor(Math.random() * (max - min)) + min
+    const length = Math.floor(Math.random() * (20 - 1)) + 1;
+
+    const mockDados = criarMockDados(length);
+
+    service.getFotos().subscribe((fotos) => {
+      expect(fotos.length).toEqual(length);
+      fotos.forEach((foto) => {
+        expect(foto.description).toContain('FOTO ');
+        expect(foto.url).toContain('https://example.com/');
+      });
+      done();
+    });
+
+    httpController.expectOne(mockDados.api).flush(mockDados.dados);
+
+    // A chamada expectOne tem que ficar depois do subscribe, que dispara a requisição para o backend.
+    // É como se o httpClient estivesse esperando agora alguém decidir qual vai ser a resposta dele.
+    // Quando essa requisição é feita, ela não vai para o backend, ela vai meio que ficar parada aguardando.
+  });
+
+  // it('',()=>{})
 });
