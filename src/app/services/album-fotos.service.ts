c13139d1185a234ebec8ac20da1c60b4c9d60e8e
FVS-MBA <feliven@gmail.com>
2025-12-20 23:04:53 -0300

    altera getFotos() para deixar descrição das fotos em letra maiúscula

--- src/app/services/album-fotos.service.ts
+++ src/app/services/album-fotos.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Foto } from '../interfaces/foto';
 import { environment } from 'src/environments/environment';
-import { delay, Observable } from 'rxjs';
+import { delay, map, Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -15,6 +15,14 @@ export class AlbumFotosService {
   getFotos(): Observable<Foto[]> {
     return this.http
       .get<Foto[]>(`${this.enderecoAPI}/photos`)
+      .pipe(
+        map((fotos) => {
+          return fotos.map((foto) => {
+            return { ...foto, description: foto.description.toUpperCase() };
+          });
+        })
+      )
+      .pipe(tap((fotos) => console.log(fotos)))
       .pipe(delay(2000));
   }
 }
